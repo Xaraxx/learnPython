0a489e9548ad22df98c53b7d96ae09cc17f7f3df
xarax <sakura-lienzo@hotmail.com>
2019-04-11 15:27:01 -0500

    new scripts for practice

---
+++ desgranado.py
@@ -0,0 +1,7 @@
+def order_desgranado(stomach,wallet):
+   hungry = stomach is 'empty'
+   hay_money = wallet is not 'empty'
+   if hungry and hay_money:
+       return hungry and hay_money
+
+#order_desgranado(stomach = 'empty', wallet = not 'empty')
---
+++ script.py
@@ -0,0 +1,4 @@
+message = 'Hello how are you?'
+
+for word in message.split():
+   print word
